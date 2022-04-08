72007ee6345d1cce5f2cf516335ffb83a207eb39
Jeff Thierch <jefferson08_alexandre@hotmail.com>
2022-04-08 10:41:34 -0300

    :sparkles: Feat: deleteProduct Controller

--- server/src/controllers/productControllers.js
+++ server/src/controllers/productControllers.js
@@ -14,8 +14,8 @@ const create = async (req, res, next) => {
 
     const createdProduct = await productsServices.create({name, code, is_active, category_id});
 
-    res.status(201).json(createdProduct);
-  
+    return res.status(201).json(createdProduct);
+
   } catch (error) {
     return next(error.message);
   }
@@ -23,14 +23,28 @@ const create = async (req, res, next) => {
 
 const update = async (req, res, next) => {
   try {
-    const { id } = req.params
+    const { id } = req.params;
     const { name, code, is_active, category_id } = req.body;
 
     const updatedProduct = await productsServices.update(
       { id, name, code, is_active, category_id }
-    )
+    );
 
-    res.status(201).json(updatedProduct);
+    return res.status(201).json(updatedProduct);
+    
+  } catch (error) {
+    return next(error.message);
+  }
+}
+
+const deleteProduct = async (req, res, next) => {
+  try {
+    const { id } = req.params;
+
+    await productsServices.deleteProduct(id);
+
+    return res.status(200).json({message: `Product with id: ${id} deleted successfully`});
+
   } catch (error) {
     return next(error.message);
   }
