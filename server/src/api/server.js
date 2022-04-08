5b5ad852d9e2354892755872b40c225fb0d8fbfe
Jeff Thierch <jefferson08_alexandre@hotmail.com>
2022-04-08 15:15:17 -0300

    :recycle: Refactor: Node ports

--- server/src/api/server.js
+++ server/src/api/server.js
@@ -1,7 +1,7 @@
 const server = require('./app')
 require('dotenv').config()
 
-const PORT = process.env.NODE_LOCAL_PORT || 3000;
+const PORT = process.env.PORT || 3000;
 
 server.listen(PORT, () => {
   console.log(`Server running in port ${PORT}`);
