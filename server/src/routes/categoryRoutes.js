7d02ac64b074263bf1528cd2e162a3a690e6953b
Jeff Thierch <jefferson08_alexandre@hotmail.com>
2022-04-08 10:51:11 -0300

    :sparkles: Feat: category routes methods

--- server/src/routes/categoryRoutes.js
+++ server/src/routes/categoryRoutes.js
@@ -2,5 +2,10 @@ const express = require('express');
 
 const router = express.Router();
 
+const categoryController = require('../controllers/categoryControllers');
+
+router
+  .get('/', categoryController.getAll)
+  .post('/', categoryController.create)
 
 module.exports = router
