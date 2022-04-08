6373a7fe85c856f42f72c013051cf55b16b03bf3
Jeff Thierch <jefferson08_alexandre@hotmail.com>
2022-04-08 10:36:11 -0300

    :sparkles: Feat: Update Product Controller

--- server/src/controllers/productControllers.js
+++ server/src/controllers/productControllers.js
@@ -6,7 +6,6 @@ const getAll = async (_req, res, next) => {
   } catch (error) {
     return next(error.message);
   }
-
 };
 
 const create = async (req, res, next) => {
@@ -15,10 +14,24 @@ const create = async (req, res, next) => {
 
     const createdProduct = await productsServices.create({name, code, is_active, category_id});
 
-    res.status(201).json(createdProduct)
+    res.status(201).json(createdProduct);
   
   } catch (error) {
-    return next(error.message)
+    return next(error.message);
   }
+};
 
+const update = async (req, res, next) => {
+  try {
+    const { id } = req.params
+    const { name, code, is_active, category_id } = req.body;
+
+    const updatedProduct = await productsServices.update(
+      { id, name, code, is_active, category_id }
+    )
+
+    res.status(201).json(updatedProduct);
+  } catch (error) {
+    return next(error.message);
+  }
 }
