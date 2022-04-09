bcb0bde8beaf8fdd5292caceaaa5432a1dc40f38
Jeff Thierch <jefferson08_alexandre@hotmail.com>
2022-04-09 15:15:25 -0300

    :sparkles: Feat: Client Docker and fix prisma

--- client/src/App.js
+++ client/src/App.js
@@ -1,13 +1,32 @@
 import logo from './logo.svg';
 import './App.css';
 
+import { useEffect } from 'react'
+
 function App() {
+
+  useEffect(() => {
+
+    const PORT = process.env.REACT_APP_SERVER_PORT
+    console.log(PORT);
+    const test = async () => {
+     const test01 = await fetch('http://localhost:3000/products');
+
+     const test02 = await test01.json();
+
+     console.log(test02);
+
+    }
+
+    test()
+  })
+
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+         { `${process.env.REACT_APP_SERVER_PORT}` }
         </p>
         <a
           className="App-link"
