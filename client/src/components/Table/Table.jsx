15c210619d0755991aedda5b5a35042ef435adc9
Jeff Thierch <jefferson08_alexandre@hotmail.com>
2022-04-10 12:54:29 -0300

    :sparkles: Feat: Table style

--- client/src/components/Table/Table.jsx
+++ client/src/components/Table/Table.jsx
@@ -5,6 +5,7 @@ import productsTableHead from '../../helpers/data';
 import { editProduct, requestDeleteProduct, requestProductsFromApi } from '../../redux/actions';
 import AplicationContext from '../../context/AplicationContext';
 import ProductNotFound from '../ProductNotFound/ProductNotFound';
+import style from './Table.module.css';
 
 export default function Table() {
   const dispatch = useDispatch();
@@ -31,15 +32,15 @@ export default function Table() {
 
   return (
     !productsData.length ? <ProductNotFound /> : (
-      <table>
-        <thead>
+      <table className={`${style.tableContainer}`}>
+        <thead className={`${style.tableTheadContainer}`}>
           <tr>
             {productsTableHead.map((head) => (
               <th key={head}>{head}</th>
             ))}
           </tr>
         </thead>
-        <tbody>
+        <tbody className={`${style.tableTbodyContainer}`}>
           {productsData.map((product) => (
             <tr key={(product.id)}>
               <td>{product.name}</td>
@@ -50,7 +51,7 @@ export default function Table() {
                 <button
                   type="button"
                   data-testid="edit-btn"
-                  className="edit-btn"
+                  className={`${style.editBtn}`}
                   disabled={isEditMode}
                   onClick={() => editProductInfos(product.id)}
                 >
@@ -60,7 +61,7 @@ export default function Table() {
                 <button
                   type="button"
                   data-testid="delete-btn"
-                  className="delete-btn"
+                  className={`${style.deleteBtn}`}
                   onClick={() => deleteProduct(product.id)}
                 >
                   <FaTrashAlt />
